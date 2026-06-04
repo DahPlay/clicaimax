c8f7d052416be5a1a3e9b7e51c24e02471970229
Chomotech <125891107+alexandreChromotech@users.noreply.github.com>
2026-06-04 16:35:09 -0300

    enum: habilitar PIX e BOLETO no BillingTypeAsaasEnum

--- app/Enums/BillingTypeAsaasEnum.php
+++ app/Enums/BillingTypeAsaasEnum.php
@@ -4,16 +4,16 @@ namespace App\Enums;
 
 enum BillingTypeAsaasEnum: string
 {
-    // case BOLETO = 'BOLETO';
     case CREDIT_CARD = 'CREDIT_CARD';
-    // case PIX = 'PIX';
+    case PIX = 'PIX';
+    case BOLETO = 'BOLETO';
 
     public function getName(): string
     {
         return match ($this) {
-            // self::BOLETO => 'BOLETO',
             self::CREDIT_CARD => 'CARTÃO DE CRÉDITO',
-            // self::PIX => 'PIX',
+            self::PIX => 'PIX',
+            self::BOLETO => 'BOLETO',
         };
     }
 }
